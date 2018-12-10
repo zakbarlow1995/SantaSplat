ea2a888a28b741f7d80458d3d263440fd5cfa905
Zak Barlow <zak@stormideas.com>
2018-12-10 12:42:34 +0000

    Added physics bodies and masks

--- SantaSplat/Scenes/GameScene.swift
+++ SantaSplat/Scenes/GameScene.swift
@@ -8,12 +8,22 @@
 
 import SpriteKit
 
+enum BucketState: Int {
+    case left, centre, right
+}
+
 class GameScene: SKScene {
     
     var bucket: SKSpriteNode!
     
     override func didMove(to view: SKView) {
+        setupPhysics()
         layoutScene()
+    }
+    
+    func setupPhysics() {
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -2.0)
+        physicsWorld.contactDelegate = self
     }
     
     func layoutScene() {
@@ -22,8 +32,13 @@ class GameScene: SKScene {
         bucket = SKSpriteNode(imageNamed: "bucket")
         bucket.size = CGSize(width: frame.size.width/4, height: frame.size.width/4)
         bucket.position = CGPoint(x: frame.midX, y: frame.minY + bucket.size.height)
-        addChild(bucket)
         
+        //Setup physics body
+        bucket.physicsBody = SKPhysicsBody(rectangleOf: bucket.size)
+        bucket.physicsBody?.categoryBitMask = PhysicsCategories.bucketCategory
+        bucket.physicsBody?.isDynamic = false
+        
+        addChild(bucket)
         spawnSanta()
     }
     
@@ -31,10 +46,33 @@ class GameScene: SKScene {
         let santa = SKSpriteNode(imageNamed: "santa")
         santa.size = CGSize(width: bucket.size.width/1.5, height: bucket.size.height/1.5)
         santa.position = CGPoint(x: frame.midX, y: frame.maxY)
+        
+        //Setup physics body
+        santa.physicsBody = SKPhysicsBody(rectangleOf: santa.size)
+        santa.physicsBody?.categoryBitMask = PhysicsCategories.santaCategory
+        
+        //Setup contact & collision test bit masks
+        santa.physicsBody?.contactTestBitMask = PhysicsCategories.bucketCategory
+        santa.physicsBody?.collisionBitMask = PhysicsCategories.none
+        
         addChild(santa)
     }
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+    }
+}
+
+extension GameScene: SKPhysicsContactDelegate {
+    //  01
+    //  10
+    //> 11
+    func didBegin(_ contact: SKPhysicsContact) {
+        let contactMask = contact.bodyA.categoryBitMask | contact.bodyB.categoryBitMask
+        
+        // Usually use a switch statement
+        if contactMask == PhysicsCategories.santaCategory | PhysicsCategories.bucketCategory {
+            print("Contact!!")
+        }
     }
 }
